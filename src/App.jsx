314dc63db35f1d64719afaf4a383550a9c65cd5f
Sayyorbek2005 <xoliqovsayyorbek@gmail.com>
2026-03-10 12:46:42 +0500

    BISMILLAH 10.03.26

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from "./pages/home/Home"
-// ? toast
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import Home from "./pages/home/Home";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import './style/StyleComponent'; // Fayl yo'lini tekshirib ol
 import Header from './components/header/Header';
+import { Button } from './style/StyleComponent';
+import { Title } from './style/StyleComponent';
+
 
 function App() {
-  
+
   return (
-    
     <div className="App">
       <ToastContainer />
-
       <Header />
+      <Title>is title tag</Title>
+      
+      <Button>enter</Button>
+
      
 
-      
-       
-    
-
-    {/* ? router */}
-
+      {/* ? router */}
       <Routes>
-        <Route path='home' element={<Home />}/>
-
+        {/* Odatda asosiy sahifa uchun path='/' bo'ladi */}
+        <Route path='/' element={<Home />} />
+        <Route path='home' element={<Home />} />
       </Routes>
-   
-      
     </div>
   );
 }
---
+++ src/style/StyleComponent.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  font-size: 2rem;
+    
+`
+
+export const Button = styled.button`
+  padding: 10px 20px;       
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    margin: 100px;
+    
+
+    &:hover {
+        background-color: #0056b3;
+    }
+`       
